09388bcf72e1dc287834b41e003718fb89fbf6ac
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-28 13:04:25 +0800

    Math Class

--- Fundamentals.java
+++ Fundamentals.java
@@ -15,17 +15,8 @@ import java.util.Arrays;
 public class Fundamentals {
 
     public static void main(String[] args) {
-        // Array is for listing items
-        int[] numbers = new int[5]; // older way for instancing an array
-        numbers[0] = 1;
-        numbers[1] = 2;
-        
-        // use this if we know ahead of time the items of an array
-        int[] numberss = {1,2,3,4};
- 
-        //numbers[10] = 3; // Exception will raise
-        System.out.println(numbers); // printing array will return string calculated based of address in memory
-        System.out.println(Arrays.toString(numbers)); // Arrays class and method to print arrays
+        int x = (int)Math.round(Math.random() * 100); // explicit casting
+        System.out.println(x);
     }
     
 }
