118c905874977cd0f10282f3231f5dea3acd7576
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-28 11:45:31 +0800

    Added different data types

--- Fundamentals.java
+++ Fundamentals.java
@@ -12,10 +12,13 @@ package fundamentals;
 public class Fundamentals {
 
     public static void main(String[] args) {
-       int myAge = 30;
-       int herAge = myAge;
-       myAge = 25;
-        System.out.println(herAge);
+       byte age = 30; // int for 4 byte (byte takes 2)
+       int viewsCount = 23_456_789; // use underscore for seperation (,)
+       long millionViews = 3_123_456_689L; // use L (l) for long
+       float price = 19.99F; // use float (F) for 4 byte. by default, java sees decimals as double. Add suffix
+       char letter = 'a'; // represents one character
+       boolean isEligible = false; // use is<varName> for naming boolean
+       
     }
     
 }
