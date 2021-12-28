acfc586980cb8bf25f7baf651f2ed0ae1ad01238
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-28 11:48:51 +0800

    Added constant

--- Fundamentals.java
+++ Fundamentals.java
@@ -12,13 +12,8 @@ package fundamentals;
 public class Fundamentals {
 
     public static void main(String[] args) {
-       byte age = 30; // int for 4 byte (byte takes 2)
-       int viewsCount = 23_456_789; // use underscore for seperation (,)
-       long millionViews = 3_123_456_689L; // use L (l) for long
-       float price = 19.99F; // use float (F) for 4 byte. by default, java sees decimals as double. Add suffix
-       char letter = 'a'; // represents one character
-       boolean isEligible = false; // use is<varName> for naming boolean
-       
+        final float PI = 3.14F;
+        // PI = 15.4F; // compilation error because it has final keyword or constant. Constant must be in capital letters
     }
     
 }
