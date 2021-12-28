1e41803625f2038e375578bbc1163c124fa0e03b
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-28 15:22:54 +0800

    Added validation in Mortgage Calculator

--- Fundamentals.java
+++ Fundamentals.java
@@ -17,19 +17,59 @@ import java.util.Scanner;
 public class Fundamentals {
 
     public static void main(String[] args) {
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        
+        int principal = 0;
+        float annualInterest = 0;
+        float monthlyInterest = 0;
+        int months = 0;
+        byte years = 0;
+        
+        
         Scanner scanner = new Scanner(System.in);
+            
+        while(true) {
+            System.out.print("Principal (1k - 1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            
+            System.out.println("Enter a number between " + numberFormat.format(1_000) + " and " + numberFormat.format(1_000_000));
+        }
         
-        System.out.print("Number: ");
-        int value = scanner.nextInt();
+        while(true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            
+            System.out.println("Enter a value greater than " + numberFormat.format(0) + " and less than or equal to " + numberFormat.format(30));
+        }
         
-        if (value % 3 == 0 && value % 5 == 0)
-            System.out.println("FizzBuzz");
-        else if (value % 3 == 0)
-            System.out.println("Buzz");
-        else if (value % 5 == 0)
-            System.out.println("Fizz");
-        else
-            System.out.println(value);
+        while(true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            
+            if (years >= 1 && years <= 30) {
+                months = years * MONTHS_IN_YEAR;
+                break;
+            }
+            
+            System.out.println("Enter a value between " + numberFormat.format(0) + " and " + numberFormat.format(30));
+        }
+        
+        double mortgage = principal * 
+                (monthlyInterest * Math.pow(1 + monthlyInterest, months) 
+                / (Math.pow(1 + monthlyInterest, months) - 1));
+        
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
     
 }
